1825b709c871b2b814e4df260e423c84ddb4e4b8
Ferdous Hossain Fahim <ferdbugs@gmail.com>
2020-02-19 08:54:23 +0800

    Sending Data in form of Dicts

--- flaskServer.py
+++ flaskServer.py
@@ -4,7 +4,10 @@ app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def handle_request():
-        print(flask.request.form)
+        performance = (flask.request.form['performance'])
+        difficulty = (flask.request.form['difficulty'])
+        print(performance)
+        print(difficulty)
         return "Hello"
 
 app.run(host="0.0.0.0", port=5000, debug=True)
